06eb7159a54a0ec364f849bf2686ca99db946138
saidoukaddour <medsaidou69@gmail.com>
2022-05-18 00:20:08 +0100

    Updates

---
+++ precache-manifest.bd8b2e0ecee9d8e3e1dc3f6be16883ca.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0462f68c9284b43cc9c",
+    "url": "/CB-app/static/css/main.81bcd78e.chunk.css"
+  },
+  {
+    "revision": "a0462f68c9284b43cc9c",
+    "url": "/CB-app/static/js/main.a0462f68.chunk.js"
+  },
+  {
+    "revision": "479ced21c4124e91b66a",
+    "url": "/CB-app/static/js/1.479ced21.chunk.js"
+  },
+  {
+    "revision": "f1e2d7c23866fdf6338b",
+    "url": "/CB-app/static/js/2.f1e2d7c2.chunk.js"
+  },
+  {
+    "revision": "e5a708c6c58aec28552f",
+    "url": "/CB-app/static/js/3.e5a708c6.chunk.js"
+  },
+  {
+    "revision": "597bb050bc1b0dd8067f",
+    "url": "/CB-app/static/js/4.597bb050.chunk.js"
+  },
+  {
+    "revision": "7ab3cb7fdc6dbabda430",
+    "url": "/CB-app/static/js/5.7ab3cb7f.chunk.js"
+  },
+  {
+    "revision": "f764d2c114d2532a19f7",
+    "url": "/CB-app/static/js/runtime~main.f764d2c1.js"
+  },
+  {
+    "revision": "fb31fc9a94cb2e10e7c3acfff91aa9fe",
+    "url": "/CB-app/static/media/logo.fb31fc9a.jpg"
+  },
+  {
+    "revision": "f9556c2074ea7e3e2a471fe1fa52aab4",
+    "url": "/CB-app/static/media/image2.f9556c20.jpg"
+  },
+  {
+    "revision": "07b1a5e9248d596877c03cb7c948c84a",
+    "url": "/CB-app/static/media/image3.07b1a5e9.jpg"
+  },
+  {
+    "revision": "2f947159c300b3330329ef62f621e022",
+    "url": "/CB-app/static/media/pellicule-facette-dentaire.2f947159.jpg"
+  },
+  {
+    "revision": "fa3c3bc442b555a9a45a7e9c5599558e",
+    "url": "/CB-app/static/media/esthetique.fa3c3bc4.jpg"
+  },
+  {
+    "revision": "f7173619d48690ac90494475f281020a",
+    "url": "/CB-app/static/media/blanchiment-dentaire-femme-sourire-400x378.f7173619.jpg"
+  },
+  {
+    "revision": "4f9fe4eb1efce42faee60276d0900887",
+    "url": "/CB-app/static/media/esthetique-profil-droit.4f9fe4eb.jpg"
+  },
+  {
+    "revision": "5b7e483b76d068744588ad18a528ee6e",
+    "url": "/CB-app/static/media/rehabilitation.5b7e483b.jpg"
+  },
+  {
+    "revision": "f9e85eb6b8d045f1c709c71577f9d0a6",
+    "url": "/CB-app/static/media/DIGITAL_SMILE_DESIGN-400x225.f9e85eb6.jpg"
+  },
+  {
+    "revision": "bcc0f107c38ca913d9a209037d0ddd78",
+    "url": "/CB-app/static/media/apropos2.bcc0f107.jpg"
+  },
+  {
+    "revision": "218bc24506a3512d8a7e9f7a60f5c034",
+    "url": "/CB-app/static/media/apropos.218bc245.jpg"
+  },
+  {
+    "revision": "7faf81cd282e65c9c0feb532fe10340e",
+    "url": "/CB-app/static/media/image7.7faf81cd.jpg"
+  },
+  {
+    "revision": "ad381dfae0bd902b10ecd179f69beb3c",
+    "url": "/CB-app/static/media/Endodoncia.ad381dfa.jpg"
+  },
+  {
+    "revision": "0d51fe4574a8ba2b1f0bbffd6f57d13e",
+    "url": "/CB-app/static/media/Plombage-dentaire.0d51fe45.jpg"
+  },
+  {
+    "revision": "a4760751efc767d606fab91c755e40ae",
+    "url": "/CB-app/static/media/inlay.a4760751.jpg"
+  },
+  {
+    "revision": "ddb1a6f1c4bb0c4a2aff748f4ff6f1cb",
+    "url": "/CB-app/static/media/couronne-dentaire-prothese-amovible.ddb1a6f1.jpg"
+  },
+  {
+    "revision": "aa86be90030743a85caf02108e7c9de9",
+    "url": "/CB-app/static/media/brige-dentaire-prothese-amovible.aa86be90.jpg"
+  },
+  {
+    "revision": "e8d93ea0edbb8da054e6bd6a749e035d",
+    "url": "/CB-app/static/media/Orthodontie-1.e8d93ea0.jpg"
+  },
+  {
+    "revision": "a80eee9b428dab676cfc1152d6e82873",
+    "url": "/CB-app/static/media/Invisalign.a80eee9b.jpg"
+  },
+  {
+    "revision": "929cf683814206621bf125ee37e3945a",
+    "url": "/CB-app/static/media/gingivite-medicale.929cf683.jpg"
+  },
+  {
+    "revision": "9489e9e25d079d66ef5049a7a910ee48",
+    "url": "/CB-app/static/media/rehabilitation-complete-sourire.9489e9e2.jpg"
+  },
+  {
+    "revision": "0956ec430fb60bf67217225c5e1bd95c",
+    "url": "/CB-app/static/media/pedodonti-1.0956ec43.jpg"
+  },
+  {
+    "revision": "cf1bfb14110762d4c3fc6f64c01a83a0",
+    "url": "/CB-app/index.html"
+  }
+];
